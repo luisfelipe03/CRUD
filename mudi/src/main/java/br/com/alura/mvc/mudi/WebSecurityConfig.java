1483f4a1e4cf91ef6b38340eef975f85afc94217
Luis Felipe <llipeandrade34@gmail.com>
2021-11-01 21:02:31 -0300

    add

--- mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,38 +1,41 @@
 package br.com.alura.mvc.mudi;
 
-import org.springframework.context.annotation.Bean;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
 			.anyRequest().authenticated()
 		.and()
-		.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
-		.logout(logout -> logout.logoutUrl("/logout"));
+		.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/usuario/pedido", true).permitAll())
+		.logout(logout -> logout.logoutUrl("/logout"))
+		.csrf().disable();
 	}
 	
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user = 
-				User.withDefaultPasswordEncoder()
-				.username("felipe")
-				.password("felipe")
-				.roles("ADM")
-				.build();
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		return new InMemoryUserDetailsManager(user);
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder);
 	}
+	
+	
 }
--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,12 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
@@ -22,26 +21,11 @@ public class HomeController {
 	private PedidoRepository pedidoRepository;
 
 	@GetMapping
-	public String home(Model model) {
+	public String home(Model model, Principal principal) {
 		
-		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
 		model.addAttribute("pedidos", pedidos);
 
 		return "home";
-	}
-	
-	@GetMapping("/{status}") 
-	public String porStatus(@PathVariable("status") String status, Model model) {
-		
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-		model.addAttribute("pedidos", pedidos);
-		model.addAttribute("status", status);
-
-		return "home";
-	}
-	
-	@ExceptionHandler(IllegalArgumentException.class)
-	public String onError() {
-		return "redirect:/home";
 	}
 }
