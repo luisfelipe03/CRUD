fb8cc8bbf3252789b006ccccfb9f6e334632b72e
Luis Felipe <llipeandrade34@gmail.com>
2022-04-09 11:22:19 -0300

    ajeitando model

--- crud/src/main/java/br/com/crud/crud/controller/crudController.java
+++ crud/src/main/java/br/com/crud/crud/controller/crudController.java
@@ -1,12 +1,29 @@
 package br.com.crud.crud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.crud.crud.model.contatos;
+import br.com.crud.crud.repository.contatoRepository;
 
 @RestController
 @RequestMapping(value = "/contatos")
 public class crudController {
 
-    
-    
+    @Autowired
+    private contatoRepository repository;
+
+    crudController(contatoRepository contatoRepository) {
+        this.repository = contatoRepository;
+    }
+
+    @GetMapping
+    public List<contatos> listar() {
+        return repository.findAll();
+    }
+
 }
--- crud/src/main/java/br/com/crud/crud/model/contatos.java
+++ crud/src/main/java/br/com/crud/crud/model/contatos.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.validation.constraints.Min;
 
 import lombok.Data;
@@ -21,17 +22,20 @@ public class contatos {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotEmpty@NotNull
     private Long id;
-    @Column(nullable = false, length = 130)
-    @Min(value = 3, message = "O nome não pode ser nulo e no minimo 3 caracteres")
+
+    @Column(nullable = false)
+    @Size(min = 3, max = 150, message = "O nome não pode ser nulo, no minimo 3 e no maximo 150 caracteres")
     private String nome;
+
     @Email(message = "Esse Email não é valido")
     @Column(nullable = false, length = 130, unique = true)
     private String email;
+
     @Column(nullable = false, length = 11)
     private String numero;
-    @Column(nullable = false, length = 3)
+    
+    @Column(nullable = false)
     @Min(value = 6, message = "Você tem que ter no minimo 6 anos")
-    @Max(value = 119, message = 
-        "Procure o guinness book porque você bateu o record de pessoa mais velha do mundo, idade maxima 119 anos")
+    @Max(value = 119, message = "Procure o guinness book porque você bateu o record de pessoa mais velha do mundo, idade maxima 119 anos")
     private int idade;
 }
